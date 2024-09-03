86663aebb33368f1a40c6c9626e448e2fc56ecde
makarenkoj <makarenkoj53@gmail.com>
2024-09-03 14:09:07 +0300

    add props, parent/child component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
 import Button from './Button/Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
   return(
@@ -11,6 +12,10 @@ function App() {
           <Button></Button>
           <Food></Food>
           <Card></Card>
+          <Student name='Spongebob' age={30} isStudent={false}></Student>
+          <Student name='Patrick' age={32} isStudent={true}></Student>
+          <Student name='Squidward' age={55} isStudent={true}></Student>
+          <Student></Student>
           <Footer></Footer>
       </>
   );
